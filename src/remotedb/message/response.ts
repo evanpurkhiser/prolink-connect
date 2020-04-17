3d0a05f14747d2b8c928cffb223814cca112a653
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-04-17 16:33:15 -0700

    Implement cue / loop / hotcue / hotloop conversion

--- src/remotedb/message/response.ts
+++ src/remotedb/message/response.ts
@@ -1,18 +1,55 @@
 import {Response} from 'src/remotedb/message/types';
 import {Field} from 'src/remotedb/fields';
 import {fieldsToItem} from 'src/remotedb/message/item';
-import {BeatGrid, WaveformDetailed, WaveformPreview, WaveformHD} from 'src/types';
+import {
+  BeatGrid,
+  WaveformDetailed,
+  WaveformPreview,
+  WaveformHD,
+  CuePoint,
+  Loop,
+  Hotcue,
+  HotLoop,
+  HotcueButton,
+} from 'src/types';
 
 /**
  * Extracts a specific bitmask, shifting it to the bitmask.
  */
-const extract = (val: number, mask: number): number =>
+const extractBitMask = (val: number, mask: number): number =>
   (val & mask) >> Math.log2(mask & -mask);
 
 /**
  * Pioneer colors are 3 bits, convert this to a percentage.
  */
-const extractColor = (val: number, mask: number): number => extract(val, mask) / 0b111;
+const extractColor = (val: number, mask: number): number =>
+  extractBitMask(val, mask) / 0b111;
+
+/**
+ * Utility to generate an filled with byte offsets for each segment
+ */
+function makeOffsetArray(byteLength: number, segmentSize: number) {
+  return new Array(byteLength / segmentSize).fill(null).map((_, i) => i * segmentSize);
+}
+
+type CueAndLoop = CuePoint | Loop | Hotcue | HotLoop;
+
+const makeCueLoopEntry = (
+  isCue: boolean,
+  isLoop: boolean,
+  frameOffset: number,
+  length: number,
+  button: false | HotcueButton
+): null | CueAndLoop =>
+  button !== false
+    ? isLoop
+      ? {type: 'hot_loop', frameOffset, length, button}
+      : {type: 'hot_cue', frameOffset, button}
+    : isLoop
+    ? {type: 'loop', frameOffset, length}
+    : isCue
+    ? {type: 'cue_point', frameOffset}
+    : null;
 
 /**
  * Generic null converter, for responses with no data.
@@ -41,13 +78,11 @@ const convertBeatGrid = (args: Field[]): BeatGrid => {
 
   type Count = BeatGrid[number]['count'];
 
-  return new Array(data.length / 0x10)
-    .fill(null)
-    .map((_, i) => i * 0x10)
-    .map(byteOffset => ({
-      offset: data.readUInt32LE(byteOffset + 1),
-      count: data[byteOffset] as Count,
-    }));
+  return makeOffsetArray(data.length, 0x10).map(byteOffset => ({
+    offset: data.readUInt32LE(byteOffset + 4),
+    bpm: data.readUInt16LE(byteOffset + 2) / 100,
+    count: data[byteOffset] as Count,
+  }));
 };
 
 /**
@@ -56,15 +91,13 @@ const convertBeatGrid = (args: Field[]): BeatGrid => {
 const convertWaveformPreview = (args: Field[]): WaveformPreview => {
   const data = args[3].value as Buffer;
 
-  const PREVIEW_WIDTH = 400;
+  // TODO: The last 100 bytes in the data array is a tiny waveform preview
+  const PREVIEW_DATA_LEN = 800;
 
-  return new Array(PREVIEW_WIDTH)
-    .fill(null)
-    .map((_, i) => i * 2)
-    .map(byteOffset => ({
-      height: data[byteOffset],
-      whiteness: data[byteOffset + 1] / 7,
-    }));
+  return makeOffsetArray(PREVIEW_DATA_LEN, 0x02).map(byteOffset => ({
+    height: data[byteOffset],
+    whiteness: data[byteOffset + 1] / 7,
+  }));
 };
 
 /**
@@ -76,9 +109,7 @@ const convertWaveformDetailed = (args: Field[]): WaveformDetailed => {
   // Every byte reperesents one segment of the waveform, and there are 150
   // segments per second of audio. (These seem to correspond to 'half frames'
   // following the seconds in the player display.) Each byte encodes both a
-  // color and height. The three high-order bits encode the color, ranging from
-  // darkest blue at 0 to near-white at 7. The five low-order bits encode the
-  // height of the waveform at that point, from 0 to 31 pixels.
+  // color and height.
   //
   // |  7  6  5  |  4  3  2  1  0 |
   // [ whiteness |     height     ]
@@ -86,7 +117,7 @@ const convertWaveformDetailed = (args: Field[]): WaveformDetailed => {
   const heightMask    = 0b00011111; //prettier-ignore
 
   return Array.from(data).map(b => ({
-    height: extract(b, heightMask),
+    height: extractBitMask(b, heightMask),
     whiteness: extractColor(b, whitenessMask),
   }));
 };
@@ -98,25 +129,99 @@ const convertWaveformHD = (args: Field[]): WaveformHD => {
   const WAVEFORM_START = 0x34;
   const data = (args[3].value as Buffer).slice(WAVEFORM_START);
 
-  // two byte bit representation
+  // TODO: This response is also used for the HD waveform previews, however
+  // those tend to have a much more complex data structure.
+
+  // Two byte bit representation for the color waveform.
   //
   // | f  e  d | c  b  a | 9  8  7 | 6  5  4  3  2 | 1   0 |
   // [   red   |  green  |   blue  |     height    | ~ | ~ ]
   const redMask    = 0b11100000_00000000; // prettier-ignore
   const greenMask  = 0b00011100_00000000; // prettier-ignore
   const blueMask   = 0b00000011_10000000; // prettier-ignore
-  const heightMask = 0b00000011_01111100; // prettier-ignore
+  const heightMask = 0b00000000_01111100; // prettier-ignore
 
   const ec = extractColor;
 
-  return new Array(data.length / 2)
-    .fill(null)
-    .map((_, i) => i * 2)
-    .map(byteOffset => data.readUInt16LE(byteOffset))
+  return makeOffsetArray(data.length, 0x02)
+    .map(byteOffset => data.readUInt16BE(byteOffset))
     .map(v => ({
-      height: extract(v, heightMask),
+      height: extractBitMask(v, heightMask),
       color: [ec(v, redMask), ec(v, greenMask), ec(v, blueMask)],
     }));
+};
+
+/**
+ * Converts old-style cue / loop / hotcue / hotloop data.
+ */
+const convertCueAndLoops = (args: Field[]): CueAndLoop[] => {
+  const data = args[3].value as Buffer;
+
+  return makeOffsetArray(data.length, 0x24)
+    .map(byteOffset => {
+      const entry = data.slice(byteOffset, byteOffset + 0x24);
+
+      const isLoop = !!entry[0];
+      const isCue = !!entry[1];
+      const button = entry[2] === 0 ? false : (entry[2] as HotcueButton);
+
+      const frameOffset = entry.readUInt32LE(0x0c);
+      const length = entry.readUInt32LE(0x10) - frameOffset;
+
+      return makeCueLoopEntry(isCue, isLoop, frameOffset, length, button);
+    })
+    .filter((c): c is CueAndLoop => c !== null);
+};
+
+/**
+ * Converts new-style cue / loop / hotcue / hotloop data, including labels and
+ * colors.
+ */
+const convertAdvCueAndLoops = (args: Field[]): CueAndLoop[] => {
+  const data = args[3].value as Buffer;
+  const entries = [];
+
+  for (let offset = 0; offset < data.length; ) {
+    const length = data.readUInt32LE(offset);
+    entries.push(data.slice(offset, offset + length));
+    offset += length;
+  }
+
+  return entries
+    .map(entry => {
+      // Deleted cue point
+      if (entry[6] === 0x00) {
+        return null;
+      }
+
+      // The layout here is minorly different from the basic cue and loops,
+      // so we unfortunately cannot reuse that logic.
+      const button = entry[4] === 0 ? false : (entry[4] as HotcueButton);
+      const isCue = entry[6] === 0x01;
+      const isLoop = entry[6] === 0x02;
+
+      const frameOffset = entry.readUInt32LE(0x0c);
+      const length = entry.readUInt32LE(0x10) - frameOffset;
+
+      const basicEntry = makeCueLoopEntry(isCue, isLoop, frameOffset, length, button);
+
+      // It seems the label may not always be included, if the entry is only 0x38
+      // bytes long, exclude color and comment
+      if (entry.length === 0x38) {
+        return basicEntry;
+      }
+
+      const labelByteLength = entry.readUInt16LE(0x48);
+      const label = entry
+        .slice(0x4a, 0x4a + labelByteLength)
+        .slice(0, -2)
+        .toString('utf16le');
+
+      const color = entry[0x4a + labelByteLength + 0x04];
+
+      return {...basicEntry, color, label};
+    })
+    .filter((c): c is CueAndLoop => c !== null);
 };
 
 export const responseTransform = {
@@ -124,12 +229,13 @@ export const responseTransform = {
   [Response.Error]: nullConverter,
   [Response.MenuHeader]: nullConverter,
   [Response.MenuFooter]: nullConverter,
+
   [Response.MenuItem]: fieldsToItem,
   [Response.Artwork]: convertArtwork,
   [Response.BeatGrid]: convertBeatGrid,
-  [Response.CueAndLoops]: nullConverter,
+  [Response.CueAndLoops]: convertCueAndLoops,
   [Response.WaveformPreview]: convertWaveformPreview,
   [Response.WaveformDetailed]: convertWaveformDetailed,
   [Response.WaveformHD]: convertWaveformHD,
-  [Response.AdvCueAndLoops]: nullConverter,
+  [Response.AdvCueAndLoops]: convertAdvCueAndLoops,
 } as const;
--- src/types.ts
+++ src/types.ts
@@ -99,7 +99,7 @@ export module CDJStatus {
 
 /**
  * A beat grid is a series of offsets from the start of the track. Each offset
- * indicates what count within the measure it is.
+ * indicates what count within the measure it is along with the bpm.
  */
 export type BeatGrid = Array<{
   /**
@@ -110,6 +110,10 @@ export type BeatGrid = Array<{
    * The count of this particular beat within the measure
    */
   count: 1 | 2 | 3 | 4;
+  /**
+   * The BPM at this beat.
+   */
+  bpm: number;
 }>;
 
 /**
@@ -158,3 +162,92 @@ export type WaveformDetailed = WaveformSegment[];
  * second of audio).
  */
 export type WaveformHD = WaveformHDSegment[];
+
+/**
+ * A hotcue button label
+ */
+export enum HotcueButton {
+  A = 1,
+  B,
+  C,
+  D,
+  E,
+  F,
+  G,
+  H,
+}
+
+/**
+ * When a custom color is not configured the cue point will be one of these
+ * colors.
+ */
+export enum CueColor {
+  None = 0x00,
+  Blank = 0x15,
+  Magenta = 0x31,
+  Violet = 0x38,
+  Fuchsia = 0x3c,
+  LightSlateBlue = 0x3e,
+  Blue = 0x01,
+  SteelBlue = 0x05,
+  Aqua = 0x09,
+  SeaGreen = 0x0e,
+  Teal = 0x12,
+  Green = 0x16,
+  Lime = 0x1a,
+  Olive = 0x1e,
+  Yellow = 0x20,
+  Orange = 0x26,
+  Red = 0x2a,
+  Pink = 0x2d,
+}
+
+/**
+ * Represnts a single cue point. On older exportts the label and color may be
+ * undefined.
+ */
+export type CuePoint = {
+  type: 'cue_point';
+  /**
+   * Numberof 1/150th second units (number of "half frames") from the start of
+   * the track.
+   */
+  frameOffset: number;
+  /**
+   * The comment associated to the cue point
+   */
+  label?: string;
+  /**
+   * RGB values of the hotcue color
+   */
+  color?: CueColor;
+};
+
+type BareCuePoint = Omit<CuePoint, 'type'>;
+
+/**
+ * A loop, similar to a cue point, but includes a length.
+ */
+export type Loop = BareCuePoint & {
+  type: 'loop';
+  /**
+   * The length in milliseconds of the loop
+   */
+  length: number;
+};
+
+/**
+ * A hotcue is like a cue point, but also includes the button it is assigned to.
+ */
+export type Hotcue = BareCuePoint & {
+  type: 'hot_cue';
+  /**
+   * Which hotcue button this hotcue is assigned to.
+   */
+  button: HotcueButton;
+};
+
+/**
+ * A hot loop, this is the union of a hotcue and a loop.
+ */
+export type HotLoop = {type: 'hot_loop'} & Omit<Hotcue & Loop, 'type'>;
