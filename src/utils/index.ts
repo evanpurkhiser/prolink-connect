03115a721aef79f84f8e46551770f73007ec1e1d
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-05-18 02:43:21 -0700

    Handle undefined networkInterfaces

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -29,7 +29,8 @@ export function buildName(device: Device): Uint8Array {
  */
 export function getMatchingInterface(ipAddr: ip.Address4) {
   const flatList = Object.entries(networkInterfaces()).reduce(
-    (acc, [name, info]) => acc.concat(info.map(i => ({...i, name}))),
+    (acc, [name, info]) =>
+      info !== undefined ? acc.concat(info.map(i => ({...i, name}))) : acc,
     [] as ({name: string} & NetworkInterfaceInfo)[]
   );
 
