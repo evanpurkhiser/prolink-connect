1e94dea6b69f4be0cd1c6d43f5be55a0c03be0a6
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-05-18 03:11:52 -0700

    Move status types to a module

---
+++ src/status/types.ts
@@ -0,0 +1,49 @@
+import {DeviceID, MediaSlot, TrackType} from 'src/types';
+
+/**
+ * Status flag bitmasks
+ */
+export enum StatusFlag {
+  OnAir = 1 << 3,
+  Sync = 1 << 4,
+  Master = 1 << 5,
+  Playing = 1 << 6,
+}
+
+/**
+ * Play state flags
+ */
+export enum PlayState {
+  Empty = 0x00,
+  Loading = 0x02,
+  Playing = 0x03,
+  Looping = 0x04,
+  Paused = 0x05,
+  Cued = 0x06,
+  Cuing = 0x07,
+  Searching = 0x09,
+  SpunDown = 0x0e,
+  Ended = 0x11,
+}
+
+/**
+ * Represents various details about the current state of the CDJ.
+ */
+export type State = {
+  deviceId: number;
+  trackId: number;
+  trackDeviceId: DeviceID;
+  trackSlot: MediaSlot;
+  trackType: TrackType;
+  playState: PlayState;
+  isOnAir: boolean;
+  isSync: boolean;
+  isMaster: boolean;
+  trackBPM: number;
+  effectivePitch: number;
+  sliderPitch: number;
+  beatInMeasure: number;
+  beatsUntilCue: number;
+  beat: number;
+  packetNum: number;
+};
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,7 @@
 import ip from 'ip-address';
 
+export * as CDJStatus from 'src/status/types';
+
 /**
  * Known device types on the network
  */
@@ -108,56 +110,6 @@ export enum TrackType {
   RB = 0x01,
   Unanalyzed = 0x02,
   AudioCD = 0x05,
-}
-
-export module CDJStatus {
-  /**
-   * Status flag bitmasks
-   */
-  export enum StatusFlag {
-    OnAir = 1 << 3,
-    Sync = 1 << 4,
-    Master = 1 << 5,
-    Playing = 1 << 6,
-  }
-
-  /**
-   * Play state flags
-   */
-  export enum PlayState {
-    Empty = 0x00,
-    Loading = 0x02,
-    Playing = 0x03,
-    Looping = 0x04,
-    Paused = 0x05,
-    Cued = 0x06,
-    Cuing = 0x07,
-    Searching = 0x09,
-    SpunDown = 0x0e,
-    Ended = 0x11,
-  }
-
-  /**
-   * Represents various details about the current state of the CDJ.
-   */
-  export type State = {
-    deviceId: number;
-    trackId: number;
-    trackDeviceId: DeviceID;
-    trackSlot: MediaSlot;
-    trackType: TrackType;
-    playState: PlayState;
-    isOnAir: boolean;
-    isSync: boolean;
-    isMaster: boolean;
-    trackBPM: number;
-    effectivePitch: number;
-    sliderPitch: number;
-    beatInMeasure: number;
-    beatsUntilCue: number;
-    beat: number;
-    packetNum: number;
-  };
 }
 
 /**
@@ -312,6 +264,6 @@ export type Hotcue = BareCuePoint & {
 /**
  * A hot loop, this is the union of a hotcue and a loop.
  */
-export type Hotloop = {type: 'hot_loop'} & Omit<Hotcue & Loop, 'type'>;
+export type Hotloop = {type: 'hot_loop'} & (Omit<Hotcue, 'type'> & Omit<Loop, 'type'>);
 
 export type CueAndLoop = CuePoint | Loop | Hotcue | Hotloop;
