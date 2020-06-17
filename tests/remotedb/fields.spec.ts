3159d9d383f17364a1490765c04836ed7050d463
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-06-17 01:45:11 -0700

    Add readField tests

--- tests/remotedb/fields.spec.ts
+++ tests/remotedb/fields.spec.ts
@@ -1,3 +1,6 @@
+import PromiseReadable from 'promise-readable';
+import {ReadableStreamBuffer} from 'stream-buffers';
+
 import * as Field from 'src/remotedb/fields';
 
 describe('UInt8', () => {
@@ -96,5 +99,46 @@ describe('Binary', () => {
 
   it('encodes and decodes', () => {
     string = new Field.Binary(Buffer.of(0x0a, 0x0b, 0x0c));
+  });
+});
+
+describe('readField', () => {
+  const streamBuffer = new ReadableStreamBuffer();
+  const socket = new PromiseReadable(streamBuffer);
+
+  it('raises an error when the wrong field is read', async () => {
+    streamBuffer.put(Buffer.of(Field.FieldType.UInt16));
+
+    await expect(async () => {
+      await Field.readField(socket, Field.FieldType.UInt8);
+    }).rejects.toThrow('Expected UInt8 but got UInt16');
+  });
+
+  it('reads a fixed size integer', async () => {
+    streamBuffer.put(Buffer.of(Field.FieldType.UInt8, 0x05));
+    const data = await Field.readField(socket, Field.FieldType.UInt8);
+
+    expect(data).toBeInstanceOf(Field.UInt8);
+    expect(data.value).toBe(0x05);
+  });
+
+  it('reads a variable sized binary field', async () => {
+    streamBuffer.put(
+      Buffer.of(Field.FieldType.Binary, 0x00, 0x00, 0x00, 0x02, 0x01, 0x02)
+    );
+    const data = await Field.readField(socket, Field.FieldType.Binary);
+
+    expect(data).toBeInstanceOf(Field.Binary);
+    expect(data.value).toBeInstanceOf(Buffer);
+    expect(data.value).toEqual(Buffer.of(0x01, 0x02));
+  });
+
+  it('does not read 0 length data of a empty binary field', async () => {
+    streamBuffer.put(Buffer.of(Field.FieldType.Binary, 0x00, 0x00, 0x00, 0x00));
+    const data = await Field.readField(socket, Field.FieldType.Binary);
+
+    expect(data).toBeInstanceOf(Field.Binary);
+    expect(data.value).toBeInstanceOf(Buffer);
+    expect(data.value).toEqual(Buffer.of());
   });
 });
