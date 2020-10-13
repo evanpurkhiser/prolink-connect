93426e0981391b94165160c23612328cc496241e
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-10-13 00:35:09 -0700

    Avoid value type overwrites

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 
-export * from './types';
 export * from './entities';
 export * from './network';
 export * from './mixstatus';
+
+// Types are exported last to avoid overwriting values with type-only exports
+export * from './types';
